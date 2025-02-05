43350873f1d1f900d7a9debec396d233f267fc0f
z44d <a71537552@gmail.com>
2025-02-05 18:06:09 +0200

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -1,15 +1,41 @@
-import requests
+import cloudscraper , random , colorama
+from bs4 import BeautifulSoup
 class Quotes:
-    def __init__(self):
-        self.author = "author"
-        self.quote = "quote"
-    def Quote(self):
-        response = requests.get('https://quotes-api-self.vercel.app/quote').json()
-        quote = response[self.quote]
-        author = response[self.author]
-        return f'''“ {quote} ” 
-                                — {author}'''
+    def __init__(
+            self,
+            quote
+        ):
+        self.scraper = cloudscraper.create_scraper()
+        self.quote = quote
+        self.random = random.randint(1,100)
 
-object = Quotes()
-get_Quote = object.Quote()
-print(get_Quote)
+    def Get_Quote(
+            self
+    ):
+        while True:
+            response = self.scraper.get(f"https://www.goodreads.com/quotes/search?page={self.random}&q={self.quote}")
+            if response.status_code != 200:
+                break
+            else:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                return soup.findAll( "div" , attrs ={ "class" : "quoteText" })
+                break
+                
+print(fr"""{colorama.Fore.RED}
+ ________                __                 
+ \_____  \  __ __  _____/  |_  ____   ______
+  /  / \  \|  |  \/  _ \   __\/ __ \ /  ___/
+ /   \_/.  \  |  (  <_> )  | \  ___/ \___ \ 
+ \_____\ \_/____/ \____/|__|  \___  >____  >    
+        \__>                      \/     \/     Join to my channel : {colorama.Fore.BLUE}t.me/PyCodz
+    
+      {colorama.Fore.RED}“ {colorama.Fore.WHITE}Welcome to Goodreads Quotes ( random ) ! {colorama.Fore.RED}”
+""")
+while True:
+    requests = input(f"{colorama.Fore.RED}> Enter your search quote : {colorama.Fore.WHITE}")
+    quotes = Quotes(requests).Get_Quote()
+    if quotes:
+        random_quote = random.choice(quotes) 
+        print(f"\n{colorama.Fore.WHITE}" , random_quote.text.strip())
+    else:
+        print("No quotes found.")
